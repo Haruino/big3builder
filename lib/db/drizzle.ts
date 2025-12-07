8ce93a686bdde8a5f95096015ae90b2a39966157
Haruino <harukiinoue5270@gmail.com>
2025-12-07 13:18:21 +0900

    script追加、フォーマット適用

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
 
 const client = postgres(process.env.DATABASE_URL!, {
   ssl: "require", // Supabaseは基本SSL必須
-});
+})
 
-export const db = drizzle(client);
+export const db = drizzle(client)
